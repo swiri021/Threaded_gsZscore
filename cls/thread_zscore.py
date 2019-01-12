805330573351d468b8388d52fde3c9a260067ea7
Jun <swiri021@gmail.com>
2019-01-11 17:38:40 -0800

    Added Thread controller

--- cls/thread_zscore.py
+++ cls/thread_zscore.py
@@ -4,23 +4,28 @@ import threading
 import functools
 
 class funcThread(object):
-    def __init__(self,  n=5):
-        self.n = n
+    def __init__(self):
+        print "Loaded Threads"
 
     def __call__(self, func):
         @functools.wraps(func)
         def run(*args, **kwargs):
+            print "Number of Threads : %d"%(kwargs['nthread'])
 
-            threads = [None]*self.n
-            container = [None]*self.n
+            threads = [None]*kwargs['nthread']
+            container = [None]*kwargs['nthread']
 
             ####Divide Samples
             i_col = len(args[1].columns.tolist())
-            contents_numb = i_col/self.n
-            data_range = range(0, i_col, contents_numb)
+            contents_numb = i_col/kwargs['nthread']
+            split_columns = [args[1].columns.tolist()[i:i+contents_numb] for i in range(0, len(args[1].columns.tolist()), contents_numb)]
 
-            for i, item in enumerate(data_range):
-                threads[i] = threading.Thread(target = func, args=(args[0], args[1].ix[:,item:item+contents_numb], container, i), kwargs=kwargs)
+            if len(split_columns)>kwargs['nthread']:
+                split_columns[len(split_columns)-2] = split_columns[len(split_columns)-2]+split_columns[len(split_columns)-1]
+                split_columns = split_columns[:len(split_columns)-1]
+
+            for i, item in enumerate(split_columns):
+                threads[i] = threading.Thread(target = func, args=(args[0], args[1].ix[:,item], container, i), kwargs=kwargs)
                 threads[i].start()
             for i in range(len(threads)):
                 threads[i].join()
@@ -36,10 +41,14 @@ class calculator(object):
         if df.empty:
             raise ValueError("Input Dataframe is empty, please try with different one.")
 
-    @funcThread(n=5)
+    # Wrapper for controlling Threads
+    def gs_zscore(self, arr1, nthread=5, gene_set=[]):
+        return self._calculating(arr1, None, None, nthread=nthread, gene_set=gene_set)
+
     # function structure
-    # args(input, container, thread_number , **kwargs)
-    def gs_zscore(self, arr1, container, i, gene_set=[] ,form='pandas'):
+    # args(input, container, thread_index , **kwargs)
+    @funcThread()
+    def _calculating(self, arr1, container, i, nthread=5, gene_set=[]):
         zscore=[]
         arr1_index = arr1.index.tolist()
         inter = list(set(arr1_index).intersection(gene_set))
@@ -51,4 +60,3 @@ class calculator(object):
         zscore.columns = ['Zscore']
         container[i] = zscore
         ##No Return
-
